9f78d34b39a0277b942d8b385cdca868ad07af8d
lait <lait.zhang@gmail.com>
2024-11-08 18:10:40 +0800

    feat: dsl

---
+++ rade-module/rade-rbac/src/main/java/org/dows/modules/rbac/controller/app/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @description: </br>
+ * @author: lait.zhang@gmail.com
+ * @date: 11/8/2024 3:32 PM
+ * @history: </br>
+ * <author>      <time>      <version>    <desc>
+ * 修改人姓名      修改时间        版本号       描述
+ */
+package org.dows.modules.rbac.controller.app;
