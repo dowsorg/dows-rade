d17edc0f7663eca91d409a6002c1bb2e424bb4d9
lait <lait.zhang@gmail.com>
2024-11-12 14:03:14 +0800

    feat: cdc

--- rade-app/src/test/java/org/dows/AppIdDataExtractor.java
+++ rade-app/src/test/java/org/dows/AppIdDataExtractor.java
@@ -2,13 +2,11 @@ package org.dows;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AppIdDataExtractor {
 
@@ -30,6 +28,8 @@ public class AppIdDataExtractor {
             // 假设要查找的appId值，这里你可以根据实际需求修改或通过参数传入
             String appIdToSearch = "12345";
 
+            String appId = "123456";
+
             for (String tableName : tableNames) {
                 // 判断表中是否存在app_id列
                 boolean hasAppIdColumn = doesTableHaveAppIdColumn(connection, tableName);
@@ -38,12 +38,12 @@ public class AppIdDataExtractor {
                     int count = countAppIdRecords(connection, tableName, appIdToSearch);
                     if (count > 0) {
                         // 生成INSERT语句，通过查询表结构获取列名来动态生成，并为列名和值加上反引号
-                        String insertStatement = generateInsertStatement(connection, tableName, appIdToSearch);
+                        String insertStatement = generateInsertStatement(connection, tableName, appIdToSearch,appId);
                         System.out.println(insertStatement);
                     }
                 } else {
                     // 如果表中不存在app_id列，生成该表的全部INSERT语句，并为列名和值加上反引号
-                    String insertStatementForAll = generateInsertStatementForAll(connection, tableName);
+                    String insertStatementForAll = generateInsertStatementForAll(connection, tableName,appId);
                     System.out.println(insertStatementForAll);
                 }
             }
@@ -91,7 +91,7 @@ public class AppIdDataExtractor {
         return 0;
     }
 
-    private static String generateInsertStatement(Connection connection, String tableName, String appIdToSearch) throws SQLException {
+    private static String generateInsertStatement(Connection connection, String tableName, String appIdToSearch,String newAppId) throws SQLException {
         // 查询表结构获取列名
         List<String> columnNames = getColumnNames(connection, tableName);
 
@@ -117,9 +117,16 @@ public class AppIdDataExtractor {
                     if (i > 0) {
                         insertStatement.append(", ");
                     }
-                    insertStatement.append("'");
+                    if(columnNames.get(i).equals("app_id")){
+                        insertStatement.append("'").append(newAppId).append("'");
+                    } else {
+                        insertStatement.append("'");
+                        insertStatement.append(resultSet.getString(columnNames.get(i)));
+                        insertStatement.append("'");
+                    }
+                    /*insertStatement.append("'");
                     insertStatement.append(resultSet.getString(columnNames.get(i)));
-                    insertStatement.append("'");
+                    insertStatement.append("'");*/
                 }
                 insertStatement.append(")");
                 firstRow = false;
@@ -129,14 +136,19 @@ public class AppIdDataExtractor {
         return insertStatement.toString();
     }
 
-    private static String generateInsertStatementForAll(Connection connection, String tableName) throws SQLException {
+    private static String generateInsertStatementForAll(Connection connection, String tableName,String appId) throws SQLException {
         // 查询表结构获取列名
         List<String> columnNames = getColumnNames(connection, tableName);
 
+        Map<String,Integer> IDS = new HashMap<>();
         StringBuilder insertStatement = new StringBuilder("INSERT INTO " + tableName + " (");
         for (int i = 0; i < columnNames.size(); i++) {
             if (i > 0) {
-            insertStatement.append(", ");
+                insertStatement.append(", ");
+            }
+            // 定位是第几个
+            if(columnNames.get(i).equals("app_id")){
+                IDS.put("app_id",i);
             }
             insertStatement.append("`").append(columnNames.get(i)).append("`");
         }
@@ -153,11 +165,16 @@ public class AppIdDataExtractor {
                 insertStatement.append("(");
                 for (int i = 0, n = columnNames.size(); i < n; i++) {
                     if (i > 0) {
-                    insertStatement.append(", ");
+                        insertStatement.append(", ");
                     }
-                    insertStatement.append("'");
-                    insertStatement.append(resultSet.getString(columnNames.get(i)));
-                    insertStatement.append("'");
+
+                    if(columnNames.get(i).equals("app_id")){
+                        insertStatement.append("'").append(appId).append("'");
+                    } else {
+                        insertStatement.append("'");
+                        insertStatement.append(resultSet.getString(columnNames.get(i)));
+                        insertStatement.append("'");
+                    }
                 }
                 insertStatement.append(")");
                 firstRow = false;
